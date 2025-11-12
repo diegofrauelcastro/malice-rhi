7fb164f2626e3532a62a9517865f054b5f2c1563
Diego FRAUEL CASTRO <d.frauel@student.isartdigital.com>
2025-11-12 19:24:35 +0100

    Add: swapchain interface and vulkan implementation
    Vulkan swapchain owns the swapchain, all its images and imageviews, the swapchain extend and image format, and also the semaphores and fences per image.

---
+++ MaliceRHI/code/include/iswapchain.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+
+
+// Forward declarations
+class VulkanSwapChain;
+class IDevice;
+class ISurface;
+struct GLFWwindow;
+
+// Swap chain interface
+class ISwapChain
+{
+protected:
+	/// Class properties ///
+
+	// Maximum number of frames that can be modified in parallel during execution of the application with this swap chain.
+	uint32_t maxFramesInFlight = 2;
+
+public:
+	// Class destructor
+	virtual ~ISwapChain() = default;
+
+
+	/// Lifetime methods ///
+
+	virtual void Create(IDevice* _device, ISurface* _surface, GLFWwindow* _window) = 0;
+	virtual void Destroy(IDevice* _device) = 0;
+
+
+	/// Retrieving the backend ///
+
+	virtual VulkanSwapChain& API_Vulkan() { throw std::runtime_error("Bad API call : object is not a VulkanSwapChain."); }
+
+
+	/// Class specific methods ///
+
+	// Returns the maximum number of frames that can be modified in parallel during execution of the application.
+	uint32_t GetMaxFramesInFlight() const { return maxFramesInFlight; }
+
+	// Gets the next presentable image in the swap chain. Returns i, the index of the retrieved image.
+	virtual uint32_t AcquireNextImage(IDevice* _device, uint32_t currentFrameIndex) = 0;
+};
--- MaliceRHI/main.cpp
+++ MaliceRHI/main.cpp
@@ -1,6 +1,7 @@
 #include "iinstance.h"
 #include "isurface.h"
 #include "idevice.h"
+#include "iswapchain.h"
 
 int main()
 {
